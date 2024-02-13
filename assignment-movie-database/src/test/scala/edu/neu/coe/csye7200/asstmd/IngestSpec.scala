fd1c1ffb0a1c7c099944d7064d27a2014bfc6876
Kavish-Daftari <114007562+Kavish-Daftari@users.noreply.github.com>
2024-02-12 20:59:28 -0500

    Final commit

--- assignment-movie-database/src/test/scala/edu/neu/coe/csye7200/asstmd/IngestSpec.scala
+++ assignment-movie-database/src/test/scala/edu/neu/coe/csye7200/asstmd/IngestSpec.scala
@@ -22,7 +22,7 @@ class IngestSpec extends AnyFlatSpec with Matchers {
     val xys = ingester(source).toSeq
     // check that xys has exactly one element, consisting of Success(42) -- 10 points
     // TO BE IMPLEMENTED 
-
+    xys should contain only Success(42)
 
     // STUB
     // END
