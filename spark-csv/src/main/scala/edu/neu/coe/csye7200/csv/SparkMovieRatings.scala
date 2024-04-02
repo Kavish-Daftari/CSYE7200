580b7195001edd6c25d487942587be8addc55139
Kavish-Daftari <114007562+Kavish-Daftari@users.noreply.github.com>
2024-04-02 08:55:39 -0400

    Final commit

---
+++ spark-csv/src/main/scala/edu/neu/coe/csye7200/csv/SparkMovieRatings.scala
@@ -0,0 +1,29 @@
+package edu.neu.coe.csye7200.csv
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+object SparkMovieRatings {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("Movie Ratings Analysis")
+      .config("spark.master", "local")
+      .getOrCreate()
+
+
+    val filePath = "/Users/kavishdaftari/Bigdata/CSYE7200/spark-csv/src/main/resources/movie_metadata.csv" // Update this path
+
+    val df = spark.read.option("header", "true").csv(filePath)
+    df.show()
+
+    val processedDf = calculateStatisticsByDirector(df)
+    processedDf.show()
+  }
+
+  def calculateStatisticsByDirector(df: org.apache.spark.sql.DataFrame): org.apache.spark.sql.DataFrame = {
+    df.groupBy("director_name")
+      .agg(
+        mean("imdb_score").alias("mean_imdb_score"),
+        stddev("imdb_score").alias("std_dev_imdb_score")
+      )
+  }
+}
